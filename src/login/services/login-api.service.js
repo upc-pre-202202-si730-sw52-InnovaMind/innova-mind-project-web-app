a8d577402217c2dc4859ac0fcbf79367137d88f9
Abel Cierto <83315281+AbelCierto@users.noreply.github.com>
2022-11-22 15:07:54 -0500

    feat(update): Added the link to obtain all the users

--- src/login/services/login-api.service.js
+++ src/login/services/login-api.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 export class loginApiService {
-    UsersURL= "https://innovamind.herokuapp.com/api/v1/users";
+    UsersURL= "https://localhost:7275/api/v1/users";
     getUsers() {
         return axios.get(this.UsersURL);
     }
