a93da6410aea8a161a6e9c6b19a44ae80f0281fa
Abel Cierto <83315281+AbelCierto@users.noreply.github.com>
2022-11-22 22:17:16 -0500

    feat(update): Added the method async to login

--- src/login/services/login-api.service.js
+++ src/login/services/login-api.service.js
@@ -1,7 +1,17 @@
 import axios from "axios";
-export class loginApiService {
+export class LoginApiService {
     UsersURL= "https://localhost:7275/api/v1/users/sign-in";
+    PostUserURL = "https://localhost:7275/api/v1/users/sign-up";
+    PostDriverURL = "https://localhost:7275/api/v1/driver";
+
     getUser(UserRequest) {
         return axios.post(this.UsersURL, UserRequest);
     }
+    postUser(UserRequest) {
+        return axios.post(this.PostUserURL, UserRequest);
+    }
+    postDriver(DriverRequest) {
+        return axios.post(this.UsersURL, DriverRequest);
+    }
+
 }
