b7466a74ade093b417917447ce1cb56dd6b38d8d
Abel Cierto <83315281+AbelCierto@users.noreply.github.com>
2022-11-24 04:58:33 -0500

    feat(update): Update the home driver

--- src/drivers/pages/home_driver.vue
+++ src/drivers/pages/home_driver.vue
@@ -1,9 +1,6 @@
 <template>
     <div class="w-full">
-        <pv-card
-            style="width: 25rem; margin-bottom: 2em; margin-top: 2em"
-            class="Welcome"
-        >
+        <pv-card style="width: 25rem; margin-bottom: 2em; margin-top: 2em" class="Welcome">
             <template #title> Welcome back! </template>
             <template #content>
                 <p class="text-white">
@@ -12,30 +9,26 @@
             </template>
         </pv-card>
 
-        <pv-card
-            style="width: 25em; margin-top: 2em"
-            v-for="(New, i) in news"
-            :key="i"
-        >
+        <pv-card style="width: 25em; margin-top: 2em" v-for="(New, i) in news" :key="i">
             <template #header>
-                <img :src="New.photo" style="height: 15rem" />
+                <img :src="photoImage" style="height:15rem">
             </template>
             <template #title> {{ New.title }} </template>
-            <template #subtitle> {{ New.user.imageUrl }} </template>
+            <template #subtitle> I am a recruiter from one {{ New.user.role }} </template>
             <template #content>
                 <p>
                     {{ New.description }}
                 </p>
             </template>
-            <template #footer>
-                <pv-button icon="pi pi-check" label="Save" />
-                <pv-button
-                    icon="pi pi-times"
-                    label="Cancel"
-                    class="p-button-secondary"
-                    style="margin-left: 0.5em"
-                />
-            </template>
+          <template #footer>
+            <pv-button icon="pi pi-eye" label="View Profile" />
+            <pv-button
+                icon="pi pi-bell"
+                label="Notify"
+                class="p-button-secondary"
+                style="margin-left:0.5em"
+            />
+          </template>
         </pv-card>
     </div>
 </template>
@@ -45,45 +38,45 @@ import { DriversServices } from "../services/drivers-api.services";
 
 export default {
     data() {
-      let map1 = new Map();
       return {
           photoImage: "https://astinaccounts.com/wp-content/uploads/2019/10/Astin-Accounts-Hiring.png",
           news: [
-            { "title": "",
-              "description": "",
+            { "title": "Need driver",
+              "description": "I am a driver",
               "user": {
-
+                "id": "",
+                "firstName": "",
+                "lastName": "",
+                "userName": "",
+                "password": "",
+                "phone": "",
+                "role": "",
+                "description": "",
+                "imageUrl": ""
               }
             }
           ],
           service: null,
-          map1 : [],
         };
     },
-    created() {
-        try {
-          try {
-            this.service = new DriversServices();
-            this.service.GetDriversNews().then((response) => {
-              if(response.status == 200){
-                this.news = response.data;
-                for (let i = 0; i < response.data.length; i++) {
-                  this.service.GetUserById(response.data[i].recruiter.userId).then((response) => {
-                    if(response.status == 200){
-                      this.news[i].user = response.data;
-                      console.log(this.news[i]);
-                    }
-                  });
+    async created() {
 
-                }
+      this.service = new DriversServices();
+      await this.service.GetDriversNews().then((response) => {
+        if(response.status == 200){
+          this.news = response.data;
+          for (let i = 0; i < response.data.length; i++) {
+            this.service.GetUserById(response.data[i].recruiter.userId).then((response) => {
+              if(response.status == 200){
+                this.news[i].user = response.data;
+                console.log(this.news[i]);
               }
             });
-          } catch (error) {
-
           }
-        } catch (error) {
-
         }
+      });
+
+
     },
     methods: {},
 };
